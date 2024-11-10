d4b09bd93e4432b904d71be0a1e0fa1c8935a2f6
niicoolaass <nicolasvieira147147@hotmail.com>
2024-11-10 20:20:15 -0300

    txt

--- script.js
+++ script.js
@@ -1,23 +1,14 @@
 $(document).ready(function () {
+    // inicia o carrossel para a classe '.card-wrapper'
     $('.card-wrapper').slick({
-        slidesToShow: 4,      // Mostra um review por vez
-        slidesToScroll: 1,    // Avança um review por vez
-        autoplaySpeed: 3000,  // Tempo entre os slides (em milissegundos)
-        dots: false,           // Exibe os indicadores de página
-        arrows: true,         // Oculta as setas de navegação
+        slidesToShow: 4, 
         prevArrow: '<img class="slick-prev" src="/img/icons/Arrow2.webp" alt="homem">',
         nextArrow: '<img class="slick-next" src="/img/icons/Arrow.webp" alt="homem">',
-
     });
-});
 
-$(document).ready(function () {
+    // inicia o carrossel para a classe '.reviews-wrapper'
     $('.reviews-wrapper').slick({
-        slidesToShow: 1,      // Mostra um review por vez
-        slidesToScroll: 1,    // Avança um review por vez
-        autoplaySpeed: 3000,  // Tempo entre os slides (em milissegundos)
-        dots: false,           // Exibe os indicadores de página
-        arrows: true,         // Oculta as setas de navegação
+        slidesToShow: 1, 
         prevArrow: '<img class="slick-prev2" src="/img/icons/Arrow2.webp" alt="homem">',
         nextArrow: '<img class="slick-next2" src="/img/icons/Arrow.webp" alt="homem">',
     });
