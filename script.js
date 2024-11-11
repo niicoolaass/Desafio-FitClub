276879b5fc508c6359b2f0eb2e931ba8efe2ddab
niicoolaass <nicolasvieira147147@hotmail.com>
2024-11-11 00:00:06 -0300

    txt

--- script.js
+++ script.js
@@ -4,6 +4,31 @@ $(document).ready(function () {
         slidesToShow: 4, 
         prevArrow: '<img class="slick-prev" src="/img/icons/Arrow2.webp" alt="homem">',
         nextArrow: '<img class="slick-next" src="/img/icons/Arrow.webp" alt="homem">',
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+        ]
     });
 
     // inicia o carrossel para a classe '.reviews-wrapper'
@@ -12,4 +37,8 @@ $(document).ready(function () {
         prevArrow: '<img class="slick-prev2" src="/img/icons/Arrow2.webp" alt="homem">',
         nextArrow: '<img class="slick-next2" src="/img/icons/Arrow.webp" alt="homem">',
     });
+
+    
 });
+
+
